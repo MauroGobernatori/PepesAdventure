6460fc0adb04fd56df97553d445b6b14c586fd31
MauroGobernatori <mauro.gobernatori@gmail.com>
2021-10-10 13:49:03 -0300

    Boton

---
+++ Assets/Scripts/activateButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class activateButton : MonoBehaviour
+{
+
+    public GameObject Player;
+
+    private Animator button;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == Player)
+        {
+           button.Play("buttonDown");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == Player)
+        {
+            button.Play("buttonUp");
+        }
+    }
+}
