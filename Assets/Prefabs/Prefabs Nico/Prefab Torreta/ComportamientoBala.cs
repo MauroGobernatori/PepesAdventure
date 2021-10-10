8faf639b917cef9e40e8a1a8ea1cc0a314f0a81b
Nicolás <nicoo.marsili@gmail.com>
2021-10-10 20:09:55 -0300

    Torreta 0.0.1

---
+++ Assets/Prefabs/Prefabs Nico/Prefab Torreta/ComportamientoBala.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComportamientoBala : MonoBehaviour
+{
+
+    public float velocidad = 20.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * velocidad * Time.deltaTime);
+    }
+
+    void OnCollisionEnter(Collision other){
+        if (other.gameObject.tag == "Player"){
+            Debug.Log("La bala choco con el player");
+            Destroy(gameObject, 0.5f);
+        }
+    }
+
+
+}
