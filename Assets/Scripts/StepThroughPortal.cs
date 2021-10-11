45c71cc2607a0172d417a8701935d4324ce3ff6f
MauroGobernatori <mauro.gobernatori@gmail.com>
2021-10-10 21:22:30 -0300

    Progreso Laser

--- Assets/Scripts/StepThroughPortal.cs
+++ Assets/Scripts/StepThroughPortal.cs
@@ -10,6 +10,8 @@ public class StepThroughPortal : MonoBehaviour
     // Para instanciar el laser
     public GameObject sphere;
 
+    private bool invocado = false;
+
     // Start is called before the first frame update
     void Start(){
         
@@ -17,7 +19,13 @@ public class StepThroughPortal : MonoBehaviour
 
     // Update is called once per frame
     void Update(){
-        
+        if (invocado)
+        {
+            if (sphere.activeInHierarchy)
+            {
+
+            }
+        }
     }
 
     void OnTriggerEnter(Collider other) {
@@ -25,12 +33,32 @@ public class StepThroughPortal : MonoBehaviour
         if(other.tag == "Player" || other.tag == "CubitoRigidBody") {
             other.transform.position = otherPortal.transform.position + otherPortal.transform.forward * 2f;
         }
-        Debug.Log("Toca");
 
         if (other.gameObject.tag == "Laser")
         {
-            Debug.Log("Toca laser");
-            sphere = Instantiate(sphere, otherPortal.transform.position, otherPortal.transform.rotation);
+            invocarLaser();
         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Laser")
+        {
+            desinvocarLaser();
+        }
+    }
+
+    void invocarLaser()
+    {
+        if (invocado) return;
+        if (!sphere.activeInHierarchy) sphere.SetActive(true);
+        sphere = Instantiate(sphere, otherPortal.transform.position + Vector3.right/2, otherPortal.transform.rotation);
+        invocado = true;
+    }
+
+    void desinvocarLaser()
+    {
+        sphere.SetActive(false);
+        invocado = false;
     }
 }
