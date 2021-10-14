6fc7558c4a016bfd5901d37ff91590b7336169f0
Nicolás <nicoo.marsili@gmail.com>
2021-10-14 09:45:23 -0300

    Personaje / Animación Salto

---
+++ Assets/Prefabs/Prefabs Nico/Prefab Personaje/ScriptPersonaje/LogicaPies.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogicaPies : MonoBehaviour
+{
+    public ComportamientoPersonaje comportamientoPersonaje;
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Suelo")
+        {
+            comportamientoPersonaje.puedoSaltar = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Suelo")
+        {
+            comportamientoPersonaje.puedoSaltar = false;
+        }
+    }
+
+}
