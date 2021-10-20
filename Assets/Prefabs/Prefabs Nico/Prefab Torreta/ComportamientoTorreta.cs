5cefbfc537c53fe0746fb72dc844d1af35733816
Nicolás <nicoo.marsili@gmail.com>
2021-10-20 13:54:12 -0300

    Torreta/Recarga - Animaciones/Camara

--- Assets/Prefabs/Prefabs Nico/Prefab Torreta/ComportamientoTorreta.cs
+++ Assets/Prefabs/Prefabs Nico/Prefab Torreta/ComportamientoTorreta.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class ComportamientoTorreta : MonoBehaviour
 {
 
-/*
-Este script se encarga de manipular el comportamiento de la torreta, asignandole un objetivo, balas, spawns de balas y demas. Cada vez que el usuario entra dentro del rango
-de disparo de la torreta ésta instancia el prefab de la bala y con el lookAt dirige la bala al objetivo.
-*/
+    /*
+    Este script se encarga de manipular el comportamiento de la torreta, asignandole un objetivo, balas, spawns de balas y demas. Cada vez que el usuario entra dentro del rango
+    de disparo de la torreta ésta instancia el prefab de la bala y con el lookAt dirige la bala al objetivo.
+    */
 
     public Transform target;
     public Transform torreta;
@@ -17,45 +17,46 @@ de disparo de la torreta ésta instancia el prefab de la bala y con el lookAt di
     public float ultimoDisparo;
     public float frecuenciaDisparo = 2.0f;
 
-    // public float tiempoDisparo = 0;
-    
+    public float tiempoDisparo = 0;
+
+    public bool dispara = true;
 
 
 
-    void Start() {
+    void Start()
+    {
         ultimoDisparo = Time.time;
-        
-        
     }
 
-    // void Update()
-    // {
-    //     tiempoDisparo = Time.time;
-    //     Debug.Log(tiempoDisparo);
-    //     if (tiempoDisparo % 8 == 0) 
-    //     {
-    //         StartCoroutine("RecargandoTorreta");
-            
-    //     }
-    // }
+    void Update()
+    {
+        tiempoDisparo = Time.time;
+        Debug.Log(Mathf.Floor(Time.time % 60 ) % 4 );
+        // Debug.Log(tiempoDisparo);
+        if (Mathf.Floor(Time.time % 60 ) % 4 == 1)
+        {
+            dispara = !dispara;
+            Debug.Log(dispara);
+        } 
 
-    void OnTriggerStay(Collider other){
-        
-        if (other.transform == target){
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+
+        if (other.transform == target)
+        {
             torreta.transform.LookAt(target);
 
-            if(ultimoDisparo < Time.time){
-                Instantiate(bala, balaSpawn.position, balaSpawn.rotation);
-                ultimoDisparo = Time.time + frecuenciaDisparo;
+            if (dispara)
+            {
+                if (ultimoDisparo < Time.time)
+                {
+                    Instantiate(bala, balaSpawn.position, balaSpawn.rotation);
+                    ultimoDisparo = Time.time + frecuenciaDisparo;
+                }
+                
             }
         }
     }
-
-    // IEnumerator RecargandoTorreta()
-    // {
-    //     yield return new WaitForSeconds(3);
-    //     Debug.Log("Torreta Recargando");
-    // }
-
-
 }
