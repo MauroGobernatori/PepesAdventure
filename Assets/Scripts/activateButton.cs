f461ea9532bcc1207d027e941e4250b6af274fe2
MauroGobernatori <mauro.gobernatori@gmail.com>
2021-10-26 18:08:54 -0300

    Progreso boton-puerta

--- Assets/Scripts/activateButton.cs
+++ Assets/Scripts/activateButton.cs
@@ -5,23 +5,68 @@ using UnityEngine;
 public class activateButton : MonoBehaviour
 {
 
-    public GameObject Player;
+    private bool bajarBoton = false;
+    private bool subirBoton = false;
+    private bool abrirPuerta = false;
+    private bool cerrarPuerta = false;
 
-    private Animator button;
+    [SerializeField] private GameObject door = null;
 
-    private void OnTriggerEnter(Collider other)
+    private void FixedUpdate()
     {
-        if (other.gameObject == Player)
+        if (bajarBoton)
         {
-           button.Play("buttonDown");
+            if(transform.localPosition.y <= -0.46)
+            {
+                abrirPuerta = true;
+                bajarBoton = false;
+                return;
+            }
+            transform.localPosition -= new Vector3(0, 1.1f, 0) * Time.deltaTime;
+        }
+        if (abrirPuerta)
+        {
+            if (door.transform.localPosition.x >= -182)
+            {
+                abrirPuerta = false;
+                return;
+            }
+            door.transform.localPosition += new Vector3(1.1f, 0, 0) * Time.deltaTime;
+        }
+        if (subirBoton)
+        {
+            if (transform.localPosition.y >= -0.4201205)
+            {
+                subirBoton = false;
+                return;
+            }
+            transform.localPosition += new Vector3(0, 1.1f, 0) * Time.deltaTime;
+        }
+        if (cerrarPuerta)
+        {
+            if (door.transform.localPosition.x  <= - 191.643)
+            {
+                cerrarPuerta = false;
+                return;
+            }
+            door.transform.localPosition -= new Vector3(1.1f, 0, 0) * Time.deltaTime;
         }
     }
 
-    private void OnTriggerExit(Collider other)
+    private void OnCollisionEnter(Collision collision)
     {
-        if (other.gameObject == Player)
+        if (collision.gameObject.GetComponent<cajaMovible>().isActiveAndEnabled)
         {
-            button.Play("buttonUp");
+            bajarBoton = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<cajaMovible>().isActiveAndEnabled)
+        {
+            subirBoton = true;
+            cerrarPuerta = true;
         }
     }
 }
